0ee752147aeb5df7dd41360c857289ecd3d3380a
DjXaTo <djxato@gmail.com>
2016-04-19 20:53:59 +0200

    version 19042016 2053

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -26,17 +26,159 @@ public class Interfaz extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        panelSuma = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtSumando1 = new javax.swing.JFormattedTextField();
+        txtSumando2 = new javax.swing.JFormattedTextField();
+        jLabel3 = new javax.swing.JLabel();
+        txtResultado = new javax.swing.JLabel();
+        btnSumar = new javax.swing.JButton();
+        panelGenerador = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        btnGenerarRandom = new javax.swing.JButton();
+        panelSubpaneles = new javax.swing.JPanel();
+        jTabbedPane2 = new javax.swing.JTabbedPane();
+        panelSub1 = new javax.swing.JPanel();
+        panelSub2 = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Sumando 1:");
+
+        jLabel2.setText("Sumando 2:");
+
+        jLabel3.setText("Resultado:");
+
+        txtResultado.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        btnSumar.setText("+");
+
+        javax.swing.GroupLayout panelSumaLayout = new javax.swing.GroupLayout(panelSuma);
+        panelSuma.setLayout(panelSumaLayout);
+        panelSumaLayout.setHorizontalGroup(
+            panelSumaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelSumaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelSumaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(panelSumaLayout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtResultado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(panelSumaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGroup(panelSumaLayout.createSequentialGroup()
+                            .addComponent(jLabel1)
+                            .addGap(18, 18, 18)
+                            .addComponent(txtSumando1, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(panelSumaLayout.createSequentialGroup()
+                            .addComponent(jLabel2)
+                            .addGap(18, 18, 18)
+                            .addComponent(txtSumando2))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnSumar, javax.swing.GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        panelSumaLayout.setVerticalGroup(
+            panelSumaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelSumaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelSumaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(panelSumaLayout.createSequentialGroup()
+                        .addGroup(panelSumaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(txtSumando1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(panelSumaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(txtSumando2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(btnSumar, javax.swing.GroupLayout.DEFAULT_SIZE, 61, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(panelSumaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 24, Short.MAX_VALUE)
+                    .addComponent(txtResultado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(165, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Suma", panelSuma);
+
+        jLabel5.setText("Aquí se generara el número random");
+
+        btnGenerarRandom.setText("Generar");
+
+        javax.swing.GroupLayout panelGeneradorLayout = new javax.swing.GroupLayout(panelGenerador);
+        panelGenerador.setLayout(panelGeneradorLayout);
+        panelGeneradorLayout.setHorizontalGroup(
+            panelGeneradorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelGeneradorLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelGeneradorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnGenerarRandom, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(panelGeneradorLayout.createSequentialGroup()
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 193, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        panelGeneradorLayout.setVerticalGroup(
+            panelGeneradorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelGeneradorLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnGenerarRandom, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(94, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Generador de números random", panelGenerador);
+
+        javax.swing.GroupLayout panelSub1Layout = new javax.swing.GroupLayout(panelSub1);
+        panelSub1.setLayout(panelSub1Layout);
+        panelSub1Layout.setHorizontalGroup(
+            panelSub1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 390, Short.MAX_VALUE)
+        );
+        panelSub1Layout.setVerticalGroup(
+            panelSub1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 244, Short.MAX_VALUE)
+        );
+
+        jTabbedPane2.addTab("tab1", panelSub1);
+
+        javax.swing.GroupLayout panelSub2Layout = new javax.swing.GroupLayout(panelSub2);
+        panelSub2.setLayout(panelSub2Layout);
+        panelSub2Layout.setHorizontalGroup(
+            panelSub2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 390, Short.MAX_VALUE)
+        );
+        panelSub2Layout.setVerticalGroup(
+            panelSub2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 244, Short.MAX_VALUE)
+        );
+
+        jTabbedPane2.addTab("tab2", panelSub2);
+
+        javax.swing.GroupLayout panelSubpanelesLayout = new javax.swing.GroupLayout(panelSubpaneles);
+        panelSubpaneles.setLayout(panelSubpanelesLayout);
+        panelSubpanelesLayout.setHorizontalGroup(
+            panelSubpanelesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jTabbedPane2)
+        );
+        panelSubpanelesLayout.setVerticalGroup(
+            panelSubpanelesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jTabbedPane2)
+        );
+
+        jTabbedPane1.addTab("Sub-paneles", panelSubpaneles);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jTabbedPane1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jTabbedPane1)
         );
 
         pack();
@@ -79,5 +221,21 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnGenerarRandom;
+    private javax.swing.JButton btnSumar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTabbedPane jTabbedPane2;
+    private javax.swing.JPanel panelGenerador;
+    private javax.swing.JPanel panelSub1;
+    private javax.swing.JPanel panelSub2;
+    private javax.swing.JPanel panelSubpaneles;
+    private javax.swing.JPanel panelSuma;
+    private javax.swing.JLabel txtResultado;
+    private javax.swing.JFormattedTextField txtSumando1;
+    private javax.swing.JFormattedTextField txtSumando2;
     // End of variables declaration//GEN-END:variables
 }
